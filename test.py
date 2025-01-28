c1eb88dd46a95625a0132dbd90a0fde5019716ac
rbo <rbenomar@gmail.com>
2025-01-28 12:26:09 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from prefect import flow, task
 
+@task(task_run_name="Task for {name}")
 @task
 def custom_task(name: str):
     print(f"Executing task: {name}")
 
-@flow
+@flow(flow_run_name="Sub flow for {name}")
 def subflow(name: str):
     print(f"Executing subflow: {name}")
     custom_task(name=f"{name}_task")
@@ -28,3 +29,15 @@ def parent_flow():
 
 if __name__ == "__main__":
     parent_flow()
+
+
+# prefect deploy test.py:parent_flow -n "DataFrameIteration" -p test
+
+
+#   After creating the deployment, you can run it using:
+
+# prefect deployment run "parent_flow/DataFrameIteration"
+
+
+#  If you want to schedule the deployment to run periodically, you can add a schedule when creating the deployment:
+# prefect deploy test.py:parent_flow -n "DataFrameIteration" -p default-agent-pool --cron "0 0 * * *"
